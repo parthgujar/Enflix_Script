d8c1f6711a0e1a1c1c333eea987361919925f9ac
parth <parth.gujar@sjsu.edu>
2018-09-18 20:51:48 -0700

    added emby customizations

--- script.sh
+++ script.sh
@@ -1,18 +1,36 @@
+#init
 apt-get -y update
 apt-get -y upgrade
 apt-get -y dist-upgrade
 apt-get -y install exfat-utils exfat-fuse git wget iptables usbmount
+
+#emby
 wget https://github.com/MediaBrowser/Emby.Releases/releases/download/3.5.2.0/emby-server-deb_3.5.2.0_armhf.deb
 dpkg -i emby-server-deb_3.5.2.0_armhf.deb
 if (( $? )); then
-	apt-get -f install
+	apt-get -f -y install
 	exit 1
 fi
+
+#ads
 mkdir /media/ads
 cd /media/ads
 wget https://github.com/parthgujar/random-files/raw/master/homepage%20video.mp4
+sleep 30
+
+
+#emby data
+wget https://github.com/parthgujar/Enflix_Script/raw/master/backup.zip
+unzip backup.zip
+cp -f -r backup/emby /var/lib
+
+
+#iptables
 iptables -A PREROUTING -t nat -i eth0 -p tcp --dport 80 -j REDIRECT --to-port 8096
 echo iptables-persistent iptables-persistent/autosave_v4 boolean true | sudo debconf-set-selections
 echo iptables-persistent iptables-persistent/autosave_v6 boolean true | sudo debconf-set-selections
 apt-get -y install iptables-persistent
+
+
+reboot
  
