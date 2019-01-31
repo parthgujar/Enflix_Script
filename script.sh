e4998724f7bfa5b3f08bf268e95094c7e96a3ade
Parth Gujar <33503888+parthgujar@users.noreply.github.com>
2019-01-30 21:30:11 -0800

    added dnsmasq

--- script.sh
+++ script.sh
@@ -5,28 +5,35 @@ apt-get -y dist-upgrade
 apt-get -y install exfat-utils exfat-fuse git wget iptables usbmount
 
 #emby
-wget https://github.com/MediaBrowser/Emby.Releases/releases/download/3.5.2.0/emby-server-deb_3.5.2.0_armhf.deb
-dpkg -i emby-server-deb_3.5.2.0_armhf.deb
-if (( $? )); then
-	apt-get -f -y install
-fi
+wget https://github.com/MediaBrowser/Emby.Releases/releases/download/4.0.1.0/emby-server-deb_4.0.1.0_armhf.deb
+dpkg -i emby-server-deb_4.0.1.0_arm64.deb
+apt-get -f -y install
+dpkg -i emby-server-deb_4.0.1.0_arm64.deb
 
-#ads
+#ads setup
 mkdir /media/ads
 cd /media/ads
 wget https://github.com/parthgujar/random-files/raw/master/homepage%20video.mp4
 sleep 30
-
 mkdir /media/usb
 
+#disabling systemctl for installing dnsmasq
+cd 
+sudo systemctl disable systemd-resolved.service
+sudo systemctl stop systemd-resolved
+rm /etc/resolv.conf
+sudo service network-manager restart
+
+#installing dnsmasq and configuring it 
+cd /etc/
+wget 
 
 
-#iptables
+
 iptables -A PREROUTING -t nat -i eth0 -p tcp --dport 80 -j REDIRECT --to-port 8096
 echo iptables-persistent iptables-persistent/autosave_v4 boolean true | sudo debconf-set-selections
 echo iptables-persistent iptables-persistent/autosave_v6 boolean true | sudo debconf-set-selections
-#apt-get -y install iptables-persistent
-# bro put this command and click yes when it asks amything 
+apt-get -y install iptables-persistent
 
 
  
