c0fe23768d8e49fa3a3d3ea2c7d124a933f0045a
Parth Gujar <33503888+parthgujar@users.noreply.github.com>
2019-02-21 11:58:53 -0800

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,8 +1,17 @@
+
+# At this point ensure that rockpi has ethernet, power and usb only connected. 
+#complete the initial setup, default password is 1234 setup new password as ghanta123
+# promt will make you create a user, name him enflix, then prompt will ask for full name etc, just hit enter till it asks you a confirmation of 'y/n ?'
+# once you have shell access, just execute the script
+
+
+sudo -s
+
 #init
 apt-get -y update
 apt-get -y upgrade
 apt-get -y dist-upgrade
-apt-get -y install exfat-utils exfat-fuse git wget iptables 
+apt-get -y install exfat-utils exfat-fuse git wget iptables usbmount autofs
 
 #emby
 wget https://github.com/MediaBrowser/Emby.Releases/releases/download/4.0.2.0/emby-server-deb_4.0.2.0_armhf.deb
@@ -14,8 +23,7 @@ dpkg -i emby-server-deb_4.0.2.0_armhf.deb
 mkdir /media/ads
 cd /media/ads
 wget https://github.com/parthgujar/random-files/raw/master/homepage%20video.mp4
-sleep 30
-mkdir /media/usb
+cd 
 mount /dev/sda1 /media/usb
 
 #disabling systemctl for installing dnsmasq
@@ -25,17 +33,27 @@ sudo systemctl stop systemd-resolved
 rm /etc/resolv.conf
 sudo service network-manager restart
 
+#at this point, connect the usb dongle to the rock as well
+
 #installing dnsmasq and configuring it 
 cd /etc/
 sudo apt-get -y install dnsmasq
-wget -o https://raw.githubusercontent.com/parthgujar/Enflix_Script/master/dnsmasq.conf
+rm -rf dnsmasq.conf
+wget https://raw.githubusercontent.com/parthgujar/Enflix_Script/master/dnsmasq.conf
 
 
 #jiofi tethering
 cd
 cd /etc/network
-wget -o https://raw.githubusercontent.com/parthgujar/Enflix_Script/master/interfaces
+rm -rf interfaces
+wget https://raw.githubusercontent.com/parthgujar/Enflix_Script/master/interfaces
 
+#at this point system will go into sleep. now is a good time to connect the ethernet cable to the netgear 'ENFLIX' router. note it is imperitive to have dhcp disabled in router settings 
+sleep 30
+
+reboot
+
+#please note, After rebooting, you need to run the following four commands
 
 #port config
 iptables -A PREROUTING -t nat -i eth0 -p tcp --dport 80 -j REDIRECT --to-port 8096
@@ -44,7 +62,6 @@ echo iptables-persistent iptables-persistent/autosave_v6 boolean true | sudo deb
 apt-get -y install iptables-persistent
 
 
-reboot
 
 
  
